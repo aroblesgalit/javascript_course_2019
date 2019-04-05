16ed9997b2a182d735af411ff5e4c267cb029387
Alvin Galit <aroblesgalit@gmail.com>
2019-04-04 19:49:05 -0700

    learned fundamental of variables and data types

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello World!!!');
+// fundamental of variables and data types
+
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28; 
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
