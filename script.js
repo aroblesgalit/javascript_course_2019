3af285690895b9a4461827c9228571a8543d2e4a
Alvin Galit <aroblesgalit@gmail.com>
2019-04-07 14:09:49 -0700

    learned Truthy and Falsy values and equality operators

--- script.js
+++ script.js
@@ -215,7 +215,7 @@ else {
 /****************************
 * The Ternary Operator and Switch Statements
 */
-
+/*
 // Ternary operator
 var firstName = 'John';
 var age = 14;
@@ -230,7 +230,7 @@ console.log(drink);
 } else {
     var drink = 'juice';
 }*/
-
+/*
 // Switch statement
 var job = 'instructor';
 switch (job) {
@@ -261,4 +261,26 @@ switch (true) {
         break;
     default:
         console.log(firstName + ' is a man.');
+}
+
+/****************************
+* Truthy and Falsy values and equality operators
+*/
+
+// falsy values: undefined, null, 0, '', NaN
+// truthy values: NOT falsy values
+
+var height;
+
+height = 23;
+
+if (height || height === 0) {
+    console.log('Variable is defined');
+} else {
+    console.log('Variable has NOT been defined');
+}
+
+// Equality operators
+if (height === '23') {
+    console.log('The == operator does type coercion!');
 }
