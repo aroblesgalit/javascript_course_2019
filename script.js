2d0d92357ad9cc8c234965a4b7750683eb5e5e49
Alvin Galit <aroblesgalit@gmail.com>
2019-04-07 19:54:57 -0700

    did coding challenge 3 with creating a function for how to calculate tips based on how much the bill is and then creating 2 arrays, 1 of just the tips, and the other of the final bills

--- script.js
+++ script.js
@@ -389,7 +389,7 @@ console.log(whatDoYouDo('retired', 'Mark'));
 /****************************
 * Arrays
 */
-
+/*
 // Initialize new array
 var names = ['John', 'Mark', 'Jane'];
 var years = new Array(1990, 1969, 1948); // Not usually used
@@ -417,3 +417,52 @@ console.log(john.indexOf(1990)); // checks if element is in the array and return
 
 var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer';
 console.log(isDesigner);
+
+
+/****************************
+* CODING CHALLENGE 3
+*/
+
+/*
+John and his family went on a holiday and went to 3 different  restaurants.
+The bills were $124, $48 and $268.
+
+To tip the waiter a fair amount, John created a simple tip calculator (as a 
+function). He likes to tip 20% of the bill when the bill is less than $50,
+15% when the bill is between $50 and $200, and 10% if the bill is more than
+$200.
+
+In the end, John would like to have 2 arrays:
+1) Containing all three tips (one for eah bill)
+2) Containing all three final paid amounts (bill + tip).
+
+(NOTE: To calculate 20% of a value, simply multiply if with 20/100 = 0.2)
+
+GOOD LUCK :)
+*/
+
+function tipCalc(bill) {
+    var percentage;
+    if (bill < 50) {
+        percentage = 0.2;
+    } else if (bill >= 50 && bill <= 200) {
+        percentage = 0.15;
+    }  else {
+        percentage = 0.1;
+    }
+    return percentage * bill;
+}
+
+var bills = [124, 48, 268];
+var tips = [
+    tipCalc(bills[0]),
+    tipCalc(bills[1]),
+    tipCalc(bills[2])
+]
+var finalValues = [
+    bills[0] + tips[0],
+    bills[1] + tips[1],
+    bills[2] + tips[2]
+]
+
+console.log(tips, finalValues);
