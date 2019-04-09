134c51ebf8d77971ca24ed16c3c88f1f111282ab
Alvin Galit <aroblesgalit@gmail.com>
2019-04-08 19:21:15 -0700

    learned about objects and properties, declared john as an object

--- script.js
+++ script.js
@@ -440,7 +440,7 @@ In the end, John would like to have 2 arrays:
 
 GOOD LUCK :)
 */
-
+/*
 function tipCalc(bill) {
     var percentage;
     if (bill < 50) {
@@ -466,3 +466,34 @@ var finalValues = [
 ]
 
 console.log(tips, finalValues);
+
+
+/****************************
+* Objects and properties
+*/
+
+// Object literal
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false
+};
+
+console.log(john.firstName);
+console.log(john['lastName']);
+var x = 'birthYear';
+console.log(john[x]);
+
+john.job = 'designer';
+john['isMarried'] = true;
+console.log(john);
+
+// new Object syntax
+var jane = new Object();
+jane.name = 'Jane';
+jane.birthYear = 1969;
+jane['lastName'] = 'Smith';
+console.log(jane);
