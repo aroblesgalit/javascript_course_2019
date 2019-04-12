ce29e1736bc7c91e9d5b51ba01103ed61d253bcc
Alvin Galit <aroblesgalit@gmail.com>
2019-04-11 21:15:09 -0700

    created a js file for notes

---
+++ notes.js
@@ -0,0 +1,16 @@
+/* 
+Section 3
+37. Execution Contexts and the Execution Stack
+
+Execution Context
+A box, container, or a wrapper which stores variables and in which a piece of our code is evaluated and executed)
+
+THE DEFAULT
+Global Execution Context
+- Code that is not inside any function
+- Associated with the global object
+- In the browser, that's the window object
+
+lastName === window.lastName   // true
+
+*/
