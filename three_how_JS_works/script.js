81fac4da62c6bea8256c463dab2e034d9595a4fd
Alvin Galit <aroblesgalit@gmail.com>
2019-04-13 22:19:51 -0700

    learned about 'this' keyword within different scopes

--- three_how_JS_works/script.js
+++ three_how_JS_works/script.js
@@ -53,7 +53,7 @@ function first() {
 
 
 // Example to show the differece between execution stack and scope chain
-
+/*
 var a = 'Hello!';
 first();
 
@@ -69,19 +69,46 @@ function first() {
 
 function third() {
     var d = 'John';
-    console.log(c);
+    // console.log(c);
+    console.log(a + d);
 }
-
+*/
 
 
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+// console.log(this);
+/*
+calculateAge(1986);
 
+function calculateAge(year) {
+    console.log(2019 - year);
+    console.log(this);
+}
+*/
 
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2019 - this.yearOfBirth);
+        /*
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+        */
+    }
+}
 
+john.calculateAge();
 
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+}
 
-
-
-
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
