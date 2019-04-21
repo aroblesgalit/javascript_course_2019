d21c41db15e497f09621dadc1085c5e7cd0cc6f5
Alvin Galit <aroblesgalit@gmail.com>
2019-04-20 20:10:28 -0700

    practiced more functions returning functions

--- five_advanced_JS/script.js
+++ five_advanced_JS/script.js
@@ -137,7 +137,7 @@ console.log(rates);
 
 
 // Lecture: Functions returning functions
-
+/*
 function interviewQuestion(job) {
     if (job === 'designer') {
         return function(name) {
@@ -164,6 +164,45 @@ designerQuestion('Mark');
 designerQuestion('Mike');
 
 interviewQuestion('teacher')('Mark');
+*/
+
+// My own practice for functions returning functions
+function workout(day) {
+    if (day === 'Monday' || day === 'Thursday') {
+        return function(time) {
+            if (time >= 6 && time <= 9) {
+                console.log('Today is ' + day + ', and it\'s only ' + time + ' o\'clock so you are working out your legs.');
+            } else {
+                console.log('Today is ' + day + ', and it\'s already ' + time + ' o\'clock so you will have to skip gym.')
+            }
+        }
+    } else if (day === 'Tuesday' || day === 'Friday') {
+        return function(time) {
+            if (time >= 6 && time <= 9) {
+                console.log('Today is ' + day + ', and it\'s only ' + time + ' o\'clock so you are working out your chest.');
+            } else {
+                console.log('Today is ' + day + ', and it\'s already ' + time + ' o\'clock so you will have to skip gym.')
+            }
+        }
+    } else if (day === 'Wednesday' || day === 'Saturday') {
+        return function(time) {
+            if (time >= 6 && time <= 9) {
+                console.log('Today is ' + day + ', and it\'s only ' + time + ' o\'clock so you are working out your back.');
+            } else {
+                console.log('Today is ' + day + ', and it\'s already ' + time + ' o\'clock so you will have to skip gym.')
+            }
+        }
+    } else {
+        return function(time) {
+            console.log('It\'s ' + time + ' o\'clock on a ' + day + '. Take a day off.')
+        }
+    }
+}
+
+workout('Tuesday')(6);
+workout('Friday')(8);
+workout('Wednesday')(10);
+workout('Sunday')(12);
 
 
 
