a6813f67faf1e48023aad96f51f6426067c994bf
Alvin Galit <aroblesgalit@gmail.com>
2019-05-01 21:02:29 -0700

    project planning and architecture

---
+++ six_budgety/app.js
@@ -0,0 +1,31 @@
+/* 
+Project Planning and Architecture: Step 1
+/*
+
+// To-Do List
+/*
+Add event handler
+Get input values
+Add the new item to our data structure
+Add the new item to the UI
+Calculate budget
+Update the UI
+*/
+
+// UI Module
+/*
+Get input values
+Add the new item to the UI
+Update the UI
+*/
+
+// Data Module
+/*
+Add the new item to our data structure
+Calculate budget
+*/
+
+// Controller
+/*
+Add event handler
+*/
