585d9453608f1621dc919da5a3b4721c1275638b
Alvin Galit <aroblesgalit@gmail.com>
2019-05-26 12:22:37 -0700

    added method in the UI controller to delete item from the UI

--- six_budgety/app.js
+++ six_budgety/app.js
@@ -162,6 +162,13 @@ var UIController = (function() {
             // Insert the HTML into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
+        
+        deleteListItem: function(selectorID) {
+            
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
+
+        },
 
         // Clear the input fields
         clearFields: function() {
@@ -269,10 +276,10 @@ var controller = (function(budgetCtrl, UICtrl) {
             budgetCtrl.deleteItem(type, ID);
 
             // 2. Delete the item from the UI
-
+            UICtrl.deleteListItem(itemID);
 
             // 3. Update and show the new budget
-
+            updateBudget();
         }
 
     };
