4d6e4f1798047326145b5964874531e0bff6bc49
Alvin Galit <aroblesgalit@gmail.com>
2019-05-27 15:23:26 -0700

    created method to display the Month and Year in the UI

--- six_budgety/app.js
+++ six_budgety/app.js
@@ -167,7 +167,8 @@ var UIController = (function() {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     };
 
     var formatNumber = function(num, type) {
@@ -283,6 +284,20 @@ var UIController = (function() {
             });
         }, 
 
+        displayMonth: function() {
+            var now, months, month, year;
+
+            now = new Date();
+            // var christmas = new Date(2019, 11, 25);
+            
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+
+            year = now.getFullYear();
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -389,6 +404,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('Appliction has started.');
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
